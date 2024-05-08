85ef2a917147792c77dd5528717a8f0be77844e0
Lios <lion.devcode@gmail.com>
2024-05-08 15:21:13 -0600

    Creacion de la app :D

---
+++ Firstiosapp/Firstiosapp/FirstiosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstiosappApp.swift
+//  Firstiosapp
+//
+//  Created by Alumno on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstiosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
